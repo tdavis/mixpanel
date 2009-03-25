d60f8a24f9d51cbde97f1eee53b19bc6210e8608
Tom Davis <tom@ticketstumbler.com>
2009-03-25 01:19:35 -0400

    Version, email changed.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup (
     name = 'mixpanel',
-    version = '0.1',
+    version = '0.2',
     description = 'Small Python library to make calls to mixpanel\'s API. Requires beanstalkd server.',
     long_description = """
     This library includes a client and a server. The client is used to queue
@@ -12,9 +12,9 @@ setup (
     server. You can get beanstalkd from http://xph.us/software/beanstalkd/.
     """,
     author = 'Tom Davis',
-    author_email = 'tom@ticketstumbler.com',
+    author_email = 'binjured@gmail.com',
     url = 'http://github.com/tdavis/mixpanel/tree/master',
-    download_url = 'http://cloud.github.com/downloads/tdavis/mixpanel/mixpanel-0.1.tar.gz',
+    download_url = 'http://cloud.github.com/downloads/tdavis/mixpanel/mixpanel-0.2.tar.gz',
     classifiers = [ 'Development Status :: 4 - Beta',
                     'License :: OSI Approved :: BSD License',
                     'Programming Language :: Python',
