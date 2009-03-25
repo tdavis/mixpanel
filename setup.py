152afeea409bd5806b850870d3c02be6adf92256
Tom Davis <tom@ticketstumbler.com>
2009-03-25 00:40:39 -0400

    Removed simplejson as a requirement; fixed formatting.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import os
 setup (
     name = 'mixpanel',
     version = '0.1',
-
     description = 'Small Python library to make calls to mixpanel\'s API. Requires beanstalkd server.',
     long_description = """
     This library includes a client and a server. The client is used to queue
@@ -12,29 +11,23 @@ setup (
     queued items out and sends the requests. Both require a running beanstalkd
     server. You can get beanstalkd from http://xph.us/software/beanstalkd/.
     """,
-
     author = 'Tom Davis',
     author_email = 'tom@ticketstumbler.com',
-
     url = 'http://github.com/tdavis/mixpanel/tree/master',
     #download_url = '',
-
     classifiers = [ 'Development Status :: 4 - Beta',
                     'License :: OSI Approved :: BSD License',
                     'Programming Language :: Python',
                     'Intended Audience :: Developers',
                     'Topic :: Software Development :: Libraries :: Python Modules',
-                    ],
-
+                  ],
     platforms = ('Any',),
     keywords = ('mixpanel',),
-
     packages = ['mixpanel',],
-    
-    package_dir = {'': '.'},
-    
-    scripts = ['scripts/mixpaneld'],
-
-    install_requires=['beanstalkc'],
-    )
+    package_dir = {'mixpanel': '.'},
+    scripts = ['scripts/mixpaneld',],
+    # beanstalkc: beanstalkd client library
+    # daemon: script used to daemonize mixpaneld script
+    install_requires=['beanstalkc', 'daemon>=1.0.1'],
+)
 
